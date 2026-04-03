74d8c7e66e0b39fc816d53ac2c8fd0310e7fbd6a
Alvaro Rivera <alvaro.srr@gmail.com>
2026-04-02 21:34:15 -0300

    US01

--- src/main/java/com/programacion4/unidad3ej3/feature/producto/mappers/ProductoMapper.java
+++ src/main/java/com/programacion4/unidad3ej3/feature/producto/mappers/ProductoMapper.java
@@ -8,9 +8,9 @@ public class ProductoMapper {
     
     public static Producto toEntity(ProductoCreateRequestDto dto) {
         Producto producto = new Producto();
-        producto.setNombre(dto.getNombre());
+        producto.setNombre(capitalize(dto.getNombre()));
         producto.setCodigo(dto.getCodigo());
-        producto.setDescripcion(dto.getDescripcion());
+        producto.setDescripcion(capitalize(dto.getDescripcion()));
         producto.setPrecio(dto.getPrecio());
         producto.setStock(dto.getStock());
         return producto;
@@ -25,5 +25,12 @@ public class ProductoMapper {
         dto.setPrecio(producto.getPrecio());
         dto.setStock(producto.getStock());
         return dto;
+    }
+
+        private static String capitalize(String texto) {
+        if (texto == null || texto.isEmpty()) return texto;
+
+        texto = texto.toLowerCase();
+        return texto.substring(0, 1).toUpperCase() + texto.substring(1);
     }
 }
